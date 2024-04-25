df0d272893ad9f10df5acae488271932da2bbc51
Wesley <wmixgamer@gmail.com>
2024-04-25 10:27:45 -0300

    teste

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57bad2f4a75917ee04dae2d404879664451cbc91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c0cf5dcbc599d2bf905e85fc0b95a0dc4ce1b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
